fc5f1309545bc3f1c786d9f421a2d792e4e8c7cd
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-07-21 17:09:09 -0400

    ajuste del menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -57,7 +57,7 @@ const linksList = [
   },
 
   {
-    title: 'Precios Criptomoneda',
+    title: 'Precios Criptomonedas',
     caption: 'Pagina de los precios de las Criptomoneda',
     link: 'prueba'
   },
