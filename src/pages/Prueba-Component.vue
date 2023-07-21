25a4483df12ab17b366ff681a6dca70569bd90c6
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2023-07-21 10:31:44 -0400

    creacion del buscador de precios

--- src/pages/Prueba-Component.vue
+++ src/pages/Prueba-Component.vue
@@ -1,4 +1,12 @@
 <template>
+<div class="search-container">
+<input type="text"
+ placeholder="Buscar moneda"
+ @keyup="searchCoin"
+ v-model="textSearch"
+ class="search-input"
+/>
+</div>
 <q-page class="flex flex-center">
 <table>
 <thead>
@@ -8,7 +16,7 @@
 </tr>
 </thead>
 <tbody>
-    <tr v-for="coin in coins" :key="coin.id">
+    <tr v-for="coin in filterCoins" :key="coin.id">
      <img :src="coin.image" style="width: 2rem;"/>
       <span>{{ coin.name }}</span>
       <td>${{ coin.current_price }}</td>
@@ -28,12 +36,36 @@ export default defineComponent({
     data() {
     return{
        coins: [],
+       filterCoins: [],
+       textSearch: '',
     };
     },
     async mounted(){
      const response =  await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
      const datos = await response.json()
      this.coins = datos;
+     this.filterCoins = datos;
     },
+
+   methods: {
+     searchCoin(){
+      this.filterCoins =  this.coins.filter(coin => coin.name.toLowerCase().includes(this.textSearch.toLowerCase()))
+     }
+   }
 });
 </script>
+
+<style>
+.search-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+
+.search-input {
+  padding: 10px;
+  width: 300px;
+  text-align: center;
+}
+</style>
